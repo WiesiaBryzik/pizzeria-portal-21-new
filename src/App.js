e14e6579807ea1f25de304572b5747a69f02d55f
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2020-03-02 11:59:57 +0100

    Add Redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Dashboard from './components/views/Dashboard/Dashboard';
@@ -10,7 +12,7 @@ import TablesBookingEdit from './components/views/TablesBookingEdit/TablesBookin
 import TablesBookingNew from './components/views/TablesBookingNew/TablesBookingNew';
 import TablesEventsEdit from './components/views/TablesEventsEdit/TablesEventsEdit';
 import TablesEventsNew from './components/views/TablesEventsNew/TablesEventsNew';
-import Waiter from './components/views/Waiter/Waiter';
+import Waiter from './components/views/Waiter/WaiterContainer.js';
 import WaiterOrderEdit from './components/views/WaiterOrderEdit/WaiterOrderEdit';
 import WaiterOrderNew from './components/views/WaiterOrderNew/WaiterOrderNew';
 
@@ -27,44 +29,45 @@ const theme = createMuiTheme({
 
 function App() {
   return (
-    <BrowserRouter basename={'/panel'}>
-      <StylesProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <MainLayout>
-            <Switch>
-              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
-              <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-              <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/bookings/booking/:id'} component={TablesBookingEdit} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/bookings/new'} component={TablesBookingNew} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/events/event/:id'} component={TablesEventsEdit} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component={TablesEventsNew} />
-              <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
-              <Route exact path={process.env.PUBLIC_URL + '/waiter/orders/order/:id'} component={WaiterOrderEdit} />
-              <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={WaiterOrderNew} />
+    <Provider store={store}>
+      <BrowserRouter basename={'/panel'}>
+        <StylesProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <MainLayout>
+              <Switch>
+                <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
+                <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+                <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+                <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+                <Route exact path={process.env.PUBLIC_URL + '/tables/bookings/booking/:id'} component={TablesBookingEdit} />
+                <Route exact path={process.env.PUBLIC_URL + '/tables/bookings/new'} component={TablesBookingNew} />
+                <Route exact path={process.env.PUBLIC_URL + '/tables/events/event/:id'} component={TablesEventsEdit} />
+                <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component={TablesEventsNew} />
+                <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+                <Route exact path={process.env.PUBLIC_URL + '/waiter/orders/order/:id'} component={WaiterOrderEdit} />
+                <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={WaiterOrderNew} />
 
-              <div className="App">
-                <header className="App-header">
-                  <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                  </p>
-                  <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Learn React Now! Right now!
-                  </a>
-                </header>
-              </div>
-            </Switch>
-          </MainLayout>
-        </ThemeProvider>
-      </StylesProvider>
-    </BrowserRouter>
-
+                <div className="App">
+                  <header className="App-header">
+                    <p>
+                      Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Learn React Now! Right now!
+                    </a>
+                  </header>
+                </div>
+              </Switch>
+            </MainLayout>
+          </ThemeProvider>
+        </StylesProvider>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -1,17 +1,118 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Waiter.module.scss';
-import { Link } from 'react-router-dom';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
-const Waiter = () => (
-  <div className={styles.component}>
-    <h2> Waiter view </h2>
+class Waiter extends React.Component {
+  static propTypes = {
+    fetchTables: PropTypes.func,
+    loading: PropTypes.shape({
+      active: PropTypes.bool,
+      error: PropTypes.anyOf(PropTypes.bool,PropTypes.string),
+    }),
+    tables: PropTypes.array,
+  }
 
-    <div>
-      <Link to={`${process.env.PUBLIC_URL}/waiter/order/new`}>New Order</Link>
-      <Link to={`${process.env.PUBLIC_URL}/waiter/orders/order/:id`}>Edit Order</Link>
+  componentDidMount(){
+    const { fetchTables } = this.props;
+    fetchTables();
+  }
 
-    </div>
-  </div>
-);
+  renderActions(status){
+    switch (status) {
+      case 'free':
+        return (
+          <>
+            <Button>thinking</Button>
+            <Button>new order</Button>
+          </>
+        );
+      case 'thinking':
+        return (
+          <Button>new order</Button>
+        );
+      case 'ordered':
+        return (
+          <Button>prepared</Button>
+        );
+      case 'prepared':
+        return (
+          <Button>delivered</Button>
+        );
+      case 'delivered':
+        return (
+          <Button>paid</Button>
+        );
+      case 'paid':
+        return (
+          <Button>free</Button>
+        );
+      default:
+        return null;
+    }
+  }
+
+  render() {
+    const { loading: { active, error }, tables } = this.props;
+
+    if(active || !tables.length){
+      return (
+        <Paper className={styles.component}>
+          <p>Loading...</p>
+        </Paper>
+      );
+    } else if(error) {
+      return (
+        <Paper className={styles.component}>
+          <p>Error! Details:</p>
+          <pre>{error}</pre>
+        </Paper>
+      );
+    } else {
+      return (
+        <Paper className={styles.component}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Table</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Order</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tables.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell>
+                    {row.status}
+                  </TableCell>
+                  <TableCell>
+                    {row.order && (
+                      <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                        {row.order}
+                      </Button>
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {this.renderActions(row.status)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      );
+    }
+  }
+}
 
 export default Waiter;
